919cb9d1c0b09bc6002d8a849614730437c84873
kesigomon <kesigomon@gmail.com>
2021-09-02 01:13:55 +0900

    wheelビルド追加

---
+++ wheels/build.sh
@@ -0,0 +1,13 @@
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+mkdir wheels
+arr=(${PLATFORM//,/ })
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+    Cont=$(docker run --platform $i -v "${PWD}/wheels":/opt/wheels -w /opt --rm -itd python:3.7-buster)
+    docker cp ../requirements.txt $Cont:/opt
+    docker exec -i $Cont \
+      /bin/bash -c "export LIBSODIUM_MAKE_ARGS=-j4; pip wheel --find-links wheels -r requirements.txt -w wheels"
+    docker rm -f $Cont
+done
